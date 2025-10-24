71a34a224678e87035f713111209177f2ffe8acc
Waruni Gunasena <121920439+WaruniGunasena@users.noreply.github.com>
2025-10-24 20:13:38 +0530

    build the controllers

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,8 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // services
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 // middlewares
+app.MapControllers();
 
 app.Run();
